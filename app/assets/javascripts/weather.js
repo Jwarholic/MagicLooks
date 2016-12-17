928af334644303a3ba7aded8c118c18b7d700b03
Jason Warholic <warholic.jason@gmail.com>
2016-12-16 18:44:51 -0800

    Include javascript file to index page clock working

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -1,27 +1,24 @@
-//  $(document).ready(function(){
-//   var lat, long;
-//   $.getJSON("http://ip-api.com/json",function(location){
-//     lat = location.lat
-//     long = location.lon
-//     $('body').append("lat: ", lat)
-//     $('body').append("<br>")
-//     $('body').append("long: ", long)
-//     $('body').append("<br>")
-//     var key = "0b00512afd38eb31d828e615774a2fd4"
-//     var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" 
-//     + lat + "&lon=" + long + "&appid=" + key
-    
-//     $.getJSON(weatherUrl,function(data){
-//     $('body').append(data.weather[0].description)
-//     var icon = data.weather[0].icon
-//     // <img id="weatherImg" src="http://openweathermap.org/img/w/" + icon + ".png"/>
-//     $('body').append('<img id="weatherImg" src="http://openweathermap.org/img/w/'+icon+'.png"/>')
-//     console.log("http://openweathermap.org/img/w/" + icon + ".png")
-//     var temp = 1.8 * (data.main.temp - 273) + 32
-//     console.log(Math.floor(temp));
- 
-//     })
+tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
 
-//   })
+function GetClock(){
+var d=new Date();
+var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getYear();
+if(nyear<1000) nyear+=1900;
+var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds(),ap;
 
-// });
+if(nhour==0){ap=" AM";nhour=12;}
+else if(nhour<12){ap=" AM";}
+else if(nhour==12){ap=" PM";}
+else if(nhour>12){ap=" PM";nhour-=12;}
+
+if(nmin<=9) nmin="0"+nmin;
+if(nsec<=9) nsec="0"+nsec;
+
+document.getElementById('clockbox').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+", "+nyear+" "+nhour+":"+nmin+":"+nsec+ap+"";
+}
+
+window.onload=function(){
+GetClock();
+setInterval(GetClock,1000);
+}
