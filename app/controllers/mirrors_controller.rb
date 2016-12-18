bd6da1a5d16faf69179ec56b90c85f7f8822f751
Jason Warholic <warholic.jason@gmail.com>
2016-12-17 16:20:46 -0800

    Add update route

--- app/controllers/mirrors_controller.rb
+++ app/controllers/mirrors_controller.rb
@@ -4,11 +4,10 @@ class MirrorsController < ApplicationController
       #@face_ids = User.select { |u| u.face_id }
       @face_ids = {}
       @face_ids["face_id"] = []
+
       User.all.each do |u|
         @face_ids["face_id"] << u.face_id
       end
-
-
 
       respond_to do |f|
         f.html { render json: @face_ids }
@@ -16,14 +15,25 @@ class MirrorsController < ApplicationController
       end
     end
 
-    def new
-        @user = User.find_by_email(params[:email])
-        if @user && @user.authenticate(params[:password])
-          # Save the user id inside the browser cookie. This is how we keep the user 
-          # logged in when they navigate around our website.
-          session[:user_id] = @user.id
-          redirect_to index
-      end
+    # def new
+    #     @user = User.find_by_email(params[:email])
+    #     if @user && @user.authenticate(params[:password])
+    #       # Save the user id inside the browser cookie. This is how we keep the user 
+    #       # logged in when they navigate around our website.
+    #       session[:user_id] = @user.id
+    #       redirect_to index
+    #   end
+    # end
+    def show
+      @mirror = Mirror.find(params[:id])
+    end
+
+    def update
+      @face_id = params[:face_id]
+      @mirror = Mirror.find(params[:mirror_id])
+
+      @mirror.update(face_id: @face_id)
+      redirect_to mirror_path(@mirror)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create]
-  resources :mirrors, only: [:index, :new]
+  resources :mirrors
   resources :users, only: [:show, :index, :new, :create, :edit, :update] do 
   end
 
