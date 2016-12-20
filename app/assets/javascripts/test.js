7abc48838502a4c2280d45318c2ec7656681d125
Jason Warholic <warholic.jason@gmail.com>
2016-12-19 20:32:38 -0800

    Hide weather key

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,49 +1,49 @@
-$(document).ready( function() {
-  var loggedIn = 'false';
-  var mirrorId = undefined;
-  var userId = undefined;
+// $(document).ready( function() {
+//   var loggedIn = 'false';
+//   var mirrorId = undefined;
+//   var userId = undefined;
 
-  $('.home-page').hide();
+//   $('.home-page').hide();
   
 
-  $.ajax({
-      url: "/mirrors",
-      method: 'get',
-      dataType: 'json'
-    })
-    .done(function(response) {
-      console.log(response);
-      mirrorId = response['mirror_id'].toString();
-      userId = response['user_id'].toString();
-    })
+//   $.ajax({
+//       url: "/mirrors",
+//       method: 'get',
+//       dataType: 'json'
+//     })
+//     .done(function(response) {
+//       console.log(response);
+//       mirrorId = response['mirror_id'].toString();
+//       userId = response['user_id'].toString();
+//     })
 
-  var logInCheck = function() {
-    $.ajax({
-      url: "/users/" + userId + "/mirrors/" + mirrorId,
-      method: 'get'
-    })
-    .done(function(response) {
+//   var logInCheck = function() {
+//     $.ajax({
+//       url: "/users/" + userId + "/mirrors/" + mirrorId,
+//       method: 'get'
+//     })
+//     .done(function(response) {
 
-      console.log("resp:",response);
+//       console.log("resp:",response);
 
-      // response["status"] && loggedIn ? $('#header').html('Hello ' + response["user_name"]) : $('#header').empty()
-      // console.log(response["user_name"]);
+//       // response["status"] && loggedIn ? $('#header').html('Hello ' + response["user_name"]) : $('#header').empty()
+//       // console.log(response["user_name"]);
 
-      if ( loggedIn !== response["status"] ) {
-        $('.home-page').toggle();
-        $('#header').html('Hello ' + response["user_name"]);
-        // console.log(response["user_name"]);
-        loggedIn = response["status"];
-      };
-    })
-    .error(function() {
-      console.log('error');
-    })
-  };
+//       if ( loggedIn !== response["status"] ) {
+//         $('.home-page').toggle();
+//         $('#header').html('Hello ' + response["user_name"]);
+//         // console.log(response["user_name"]);
+//         loggedIn = response["status"];
+//       };
+//     })
+//     .error(function() {
+//       console.log('error');
+//     })
+//   };
 
-  setInterval(function() {
-    logInCheck();
-    // console.log('its working');
-  }, 200);
+//   setInterval(function() {
+//     logInCheck();
+//     // console.log('its working');
+//   }, 200);
 
-});
+// });
