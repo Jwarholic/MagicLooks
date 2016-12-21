d5f72c67226a4927dfaf61c140fdb1fa8ed7ea7f
collinl34 <collinlshelby@gmail.com>
2016-12-21 10:53:51 -0800

    news helper code cleanup

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -2,17 +2,17 @@ module NewsHelper
 	def self.news 
 		url = "https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey="
 		key = ENV['NEWS']
-		newsUrl = url + key
-		results = HTTParty.get(newsUrl)
+		news_url = url + key
+		response = HTTParty.get(news_url)
 	end
 
 	def self.titles
-		titles = news["articles"]
-		title_string = ""
-		titles.each do |title|
-			title_string << title["title"] + ". "
+		current_news_titles = news["articles"]
+		title_heading = ""
+		current_news_titles.each do |title|
+			title_heading << title["title"] + ". "
 		end
-		title_string
+		title_heading
 	end
 
 end
