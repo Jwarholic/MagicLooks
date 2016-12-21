46670aff2839eed2c2ae2018cda09ee23cf55890
Dawit Hailu <dawithailu@Dawits-MacBook-Pro.local>
2016-12-21 12:27:29 -0800

    add polling ajax file

--- app/assets/javascripts/pollingAjax.js
+++ app/assets/javascripts/pollingAjax.js
@@ -4,48 +4,6 @@ $(document).ready( function() {
   var ownerId = undefined;
 
  $('.home-page').toggle();
-
-//   $.ajax({
-//       url: "/mirrors",
-//       method: 'get',
-//       dataType: 'json'
-//     })
-//     .done(function(response) {
-//       console.log(response);
-//       mirrorId = response['mirror_id'].toString();
-//       userId = response['user_id'].toString();
-//     })
-
-//   var logInCheck = function() {
-//     $.ajax({
-//       url: "/users/" + userId + "/mirrors/" + mirrorId,
-//       method: 'get'
-//     })
-//     .done(function(response) {
-
-//       console.log("resp:",response);
-
-//       // response["status"] && loggedIn ? $('#header').html('Hello ' + response["user_name"]) : $('#header').empty()
-//       // console.log(response["user_name"]);
-
-//       if ( loggedIn !== response["status"] ) {
-//         $('.home-page').toggle();
-//         $('#header').html('Hello ' + response["user_name"]);
-//         // console.log(response["user_name"]);
-//         loggedIn = response["status"];
-//       };
-//     })
-//     .error(function() {
-//       console.log('error');
-//     })
-//   };
-
-//   setInterval(function() {
-//     logInCheck();
-//     // console.log('its working');
-//   }, 200);
-
-// });
   $.ajax({
       url: "/mirrors",
       method: 'get',
@@ -67,7 +25,6 @@ $(document).ready( function() {
     .done(function(response) {
 
       var res = JSON.parse(response)
-
       if ( loggedIn != res.status ) {
         
         $('#header').empty()
