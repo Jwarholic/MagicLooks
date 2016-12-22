1294cc309acad2da0d9269da7a42c1b9a51b19e6
collinl34 <collinlshelby@gmail.com>
2016-12-22 09:35:16 -0800

    code cleanup and refactor

--- app/controllers/user_mirrors_controller.rb
+++ app/controllers/user_mirrors_controller.rb
@@ -12,20 +12,18 @@ class UserMirrorsController < ApplicationController
   end
 
   def new
+    @user_mirror = UserMirror.new
   end
 
   def create
     @user = User.find_by(name: params[:user_mirror][:name])
     @mirror = Mirror.find(session[:mirror_id])
-    @user_mirror = UserMirror.new(user_id: @user.id, mirror_id: @mirror.id)
+    @user_mirror = UserMirror.new(name: params[:user_mirror][:name], user_id: @user.id, mirror_id: @mirror.id)
 
     if @user_mirror.save
       redirect_to @user
     else
-      respond_to do |f|
-        f.html { render 'users-mirrors'}
-        f.js {}
-      end
+      redirect_to @user
     end
   end
 
