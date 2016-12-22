2b78733beb68e42a074c3ceb15d34783774b1a90
Brandon Huang <bhuang111@yahoo.com>
2016-12-21 18:36:40 -0800

    add ajax to the form

--- app/controllers/user_mirrors_controller.rb
+++ app/controllers/user_mirrors_controller.rb
@@ -14,12 +14,26 @@ class UserMirrorsController < ApplicationController
   end
 
   def new
-    @user = User.new()
+    @user_mirror = UserMirror.new()
+    # respond_to do |f|
+    #   f.html { render 'new'}
+    #   f.js {}
+    # end
   end
 
   def create
-    @user = User.find_by(name: params[:name])
-    @mirror = Mirror.find(params[:id])
+    p "*" * 77
+    p "params   : "
+    p params[:user_mirror][:name]
+    p "*" * 77
+    @user = User.find_by(name: params[:user_mirror][:name])
+    @mirror = Mirror.find(session[:mirror_id])
+    p "*" * 59
+    p @user
+    p "*" * 59
+    p "L" * 88
+    p @mirror
+    p "L" * 88
     @user_mirror = UserMirror.new(user_id: @user.id, mirror_id: @mirror.id)
 
     if @user_mirror.save
